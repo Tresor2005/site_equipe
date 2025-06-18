e93990f92713a7662b47ed656eb662ec8f06bcc4
Tresor2005 <164601369+Tresor2005@users.noreply.github.com>
2025-06-18 10:22:13 +0100

    Update TeamSection.tsx

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -108,7 +108,7 @@ const TeamSection = () => {
           </p>
         </div>
 
-       <center> <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
           {teamMembers.map((member) => (
             <TeamMemberCard key={member.id} member={member} />
           ))}
